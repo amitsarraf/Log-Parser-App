400b252666095629cc1079be12dc385d209fb52f
amit sarraf <amitsarraf80@gmail.com>
2022-08-11 19:25:24 +0545

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './App.css'
+
 
 function App() {
+  const [file, setfile] = useState("")
+  const [data, setData] = useState([])
+
+ const handleCLick = async() =>{
+    const formData = new FormData()
+    formData.append("file", file)
+    const data = await fetch('/api',{
+      body:formData,
+      method: 'POST',
+    })
+    const output = await data.json()
+    setData(output)
+    exportToJson()
+  }
+
+  const downloadFile = ({ data, fileName, fileType }) => {
+    // Create a blob with the data we want to download as a file
+    const blob = new Blob([data], { type: fileType })
+    // Create an anchor element and dispatch a click event on it
+    // to trigger a download
+    const a = document.createElement('a')
+    a.download = fileName
+    a.href = window.URL.createObjectURL(blob)
+    const clickEvt = new MouseEvent('click', {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    })
+    a.dispatchEvent(clickEvt)
+    a.remove()
+  }
+
+  const exportToJson = (e) => {
+    downloadFile({
+      data: JSON.stringify(data),
+      fileName: 'data.json',
+      fileType: 'text/json',
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <h1 className="header">Log Parser App</h1>
+			<div className="App">
+     <input type="file" name="file" className="custom-file-upload" onChange={(e)=>setfile(e.target.files[0])}/><br/><br/>
+				<button className="button" onClick={handleCLick}>Download</button>
+			</div>
+      
+      {console.log(data.length)}
+      {data.length > 0 && <div className="list">
+            {data.map((el, i)=>{
+              return (<ul key={el.transactionId}>
+                <li>{el.timestamp}</li>
+                <li>{el.transactionId.replace(/"/g,"")}</li>
+                <li>{el.level}</li>
+                <li>{el.msg.split("}")}</li>
+              </ul>
+              )
+            })}
+      </div>}
     </div>
+
+
   );
 }
 
